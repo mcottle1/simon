d5212b588a3eefe7c7bff34fa96ed190ee618f12
mcottle1 <kenzcottle@gmail.com>
2023-03-10 14:46:37 -0700

    Index.js updates

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const app = express();
+
+const port = process.argv.length > 2 ? process.argv[2] : 3000;
+
+app.use(express.json());
+
+app.use(express.static('public'));
+
+var apiRouter = express.Router();
+app.use(`/api`, apiRouter);
+
+apiRouter.get('/scores', (_req, res) => {
+    res.send(scores);
+});
+
+apiRouter.post('/score', (req, res) => {
+    scores = updateScores(req.body, scores);
+    res.send(scores);
+});
+
+app.use((_req, res) => {
+    res.sendFile('index.html', { root: 'public' });
+});
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
+
+let scores = [];
+function updateScores(newScore, scores) {
+  let found = false;
+  for (const [i, prevScore] of scores.entries()) {
+    if (newScore.score > prevScore.score) {
+      scores.splice(i, 0, newScore);
+      found = true;
+      break;
+    }
+  }
+
+  if (!found) {
+    scores.push(newScore);
+  }
+
+  if (scores.length > 10) {
+    scores.length = 10;
+  }
+
+  return scores;
+}
